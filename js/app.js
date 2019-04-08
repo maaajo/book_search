0d5db425be989a8a454b769303861d0f35eb029e
maaajo <matemajch@gmail.com>
2019-04-08 19:36:26 +0200

    added comments to js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 this.searchButton = document.querySelector('#search-button');
 
-class BooksData {
+class Books {
 	constructor() {
 		this.startIndex = 0;
 		this.totalItemsFoundInAPI = 0;
@@ -10,11 +10,16 @@ class BooksData {
 	}
 
 	runDataLoad(scrolled) {
+		// create string with all words written by user joined with +
+		// then Google Books API looks for each word seaparated by + not the exact phrase
 		const searchTerm = document.querySelector('#input-search').value.split(' ').join('+');
 		if (searchTerm) {
+			// on scroll add startIndex API position by 10 to load more books
 			if (scrolled) {
 				this.startIndex += 10;
 				this.loadData(searchTerm);
+				// else is added in cases when user clicks on press enter to search new or same phrase
+				// then new content is visible rather then adding new content to already existing
 			} else {
 				this.output = '';
 				this.loadData(searchTerm);
@@ -25,9 +30,15 @@ class BooksData {
 	}
 
 	checkIfScrollHitBottom() {
+		// checks the actual scroll position
+		// this is a bit of a hack to assure work on Chrome, Firefox and IE
 		const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+		// actual page height
+		// accounting for cases where html/body are set to height:100%
 		const scrollHeight =
 			(document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+		// checker whether user scrolled to bottom of the page
+		// There can be some problems due to scroll bar thats why Math.ceil and >=
 		const scrolledToBottom = Math.ceil(scrollTop + window.innerHeight) >= scrollHeight;
 
 		if (scrolledToBottom && this.startIndex <= this.totalItemsFoundInAPI) {
@@ -38,10 +49,13 @@ class BooksData {
 	loadData(search) {
 		const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=10&startIndex=${this
 			.startIndex}&orderBy=newest&fields=totalItems,items(volumeInfo/title,volumeInfo/authors,volumeInfo/industryIdentifiers,volumeInfo/imageLinks,volumeInfo/description,volumeInfo/publishedDate)`;
+		// start pulling data from API endpoint
 		fetch(endpoint)
 			.then((resp) => resp.json())
 			.then((data) => {
 				this.totalItemsFoundInAPI = data['totalItems'];
+				// iterate through each object in result
+				// and create output out of each object property
 				data.items.forEach((item) => {
 					this.output += `
                     <div class="book-wrapper">
@@ -63,6 +77,7 @@ class BooksData {
 	}
 
 	getTitle(data) {
+		// checks whether title information exists in passed object
 		if (data['volumeInfo']['title']) {
 			return data['volumeInfo']['title'];
 		} else {
@@ -71,6 +86,8 @@ class BooksData {
 	}
 
 	getISBN(data) {
+		// checks whether ISBN information exists in passed object
+		// if yes then filter ISBN information to include only ISBN_13
 		if (data['volumeInfo']['industryIdentifiers'][0]['identifier']) {
 			const filteredISBN = data['volumeInfo']['industryIdentifiers'].filter(
 				(ident) => ident.type.toLowerCase() === 'isbn_13'
@@ -82,6 +99,8 @@ class BooksData {
 	}
 
 	getDescription(data) {
+		// checks whether description information exists in passed object
+		// return only first 40 words as a description
 		if (data['volumeInfo']['description']) {
 			return data['volumeInfo']['description'].split(' ').slice(0, 39).join(' ') + '...';
 		} else {
@@ -90,6 +109,7 @@ class BooksData {
 	}
 
 	getThumbnail(data) {
+		// checks whether thumbnail link exists in passed object
 		if (data['volumeInfo']['imageLinks']) {
 			return data['volumeInfo']['imageLinks']['thumbnail'];
 		} else {
@@ -98,6 +118,8 @@ class BooksData {
 	}
 
 	getAuthor(data) {
+		// checks whether author information exists in passed object
+		// return each author on the same line separated by comma
 		if (data['volumeInfo']['authors']) {
 			return data['volumeInfo']['authors'].join(', ');
 		} else {
@@ -106,6 +128,7 @@ class BooksData {
 	}
 
 	getPublishedDate(data) {
+		// checks whether publishing date exists in passed object
 		if (data['volumeInfo']['publishedDate']) {
 			return data['volumeInfo']['publishedDate'];
 		} else {
@@ -114,11 +137,15 @@ class BooksData {
 	}
 }
 
-const book = new BooksData();
+const book = new Books();
 
+// added scroll event to window
 window.addEventListener('scroll', function() {
+	// checks whether user scrolled to bottom of the page
+	// and adds new content
 	book.checkIfScrollHitBottom();
 });
+
 // add search functionality when enter is pressed
 document.addEventListener('keydown', function(e) {
 	if (e.keyCode === 13) {
@@ -126,6 +153,7 @@ document.addEventListener('keydown', function(e) {
 	}
 });
 
+// run loading data on click
 searchButton.addEventListener('click', function() {
 	book.runDataLoad(false);
 });
