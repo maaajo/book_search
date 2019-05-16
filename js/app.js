1e39ab97ed38c42ffcafe16b676af9b97989584f
maaajo <matemajch@gmail.com>
2019-05-16 20:01:28 +0200

    changed how search starts

--- js/app.js
+++ js/app.js
@@ -1,94 +1,98 @@
-class Books {
-  constructor(root) {
-    this.root = root;
-    this.startIndex = 0;
-    this.totalItemsFoundInAPI = 0;
-    this.output = ``;
-    this.searchButton = document.querySelector('#search-button');
-  }
+{
+  class Books {
+    constructor(root) {
+      this.root = root;
+      this.startIndex = 0;
+      this.totalItemsFoundInAPI = 0;
+      this.output = ``;
+      this.searchButton = this.root.querySelector('#search-button');
+    }
 
-  runDataLoad(scrolled) {
-    // create string with all words written by user joined with +
-    // then Google Books API looks for each word separated by + not the exact phrase
-    const searchTerm = document
-      .querySelector('#input-search')
-      .value.split(' ')
-      .join('+');
-    if (searchTerm) {
+    checkIfScrollHitBottom() {
+      // checks the actual scroll position
+      // this is a bit of a hack to assure work on Chrome, Firefox and IE
+      const scrollTop =
+        (document.documentElement && document.documentElement.scrollTop) ||
+        document.body.scrollTop;
+      // actual page height
+      // accounting for cases where html/body are set to height:100%
+      const scrollHeight =
+        (document.documentElement && document.documentElement.scrollHeight) ||
+        document.body.scrollHeight;
+      // checker whether user scrolled to bottom of the page
+      // There can be some problems due to scroll bar thats why Math.ceil and >=
+      const scrolledToBottom =
+        Math.ceil(scrollTop + window.innerHeight) >= scrollHeight;
+
+      if (scrolledToBottom && this.startIndex <= this.totalItemsFoundInAPI) {
+        this.performSearch(true);
+      }
+    }
+
+    async runScrollAdd(searchTerm) {
+      this.startIndex += 10;
+      this.addBooksToPage(await this.fetchData(searchTerm));
+    }
+
+    async runNewSearch(searchTerm) {
+      this.output = '';
+      this.startIndex = 0;
+      this.root.querySelector('#results').innerHTML = '';
+      this.addBooksToPage(await this.fetchData(searchTerm));
+    }
+
+    async performSearch(scrolled) {
+      // create string with all words written by user joined with +
+      // then Google Books API looks for each word separated by + not the exact phrase
       // on scroll add startIndex API position by 10 to load more books
       if (scrolled) {
-        this.startIndex += 10;
-        this.loadData(searchTerm);
+        await this.runScrollAdd(this.searchTerm);
         // else is added in cases when user clicks on press enter to search new or same phrase
         // then new content is visible rather then adding new content to already existing
       } else {
-        this.output = '';
-        this.loadData(searchTerm);
+        this.searchTerm = this.root
+          .querySelector('#input-search')
+          .value.split(' ')
+          .join('+');
+        this.searchTerm
+          ? await this.runNewSearch(this.searchTerm)
+          : alert('Please type searched phrase!');
       }
-    } else {
-      alert('Please type searched phrase!');
     }
-  }
 
-  checkIfScrollHitBottom() {
-    // checks the actual scroll position
-    // this is a bit of a hack to assure work on Chrome, Firefox and IE
-    const scrollTop =
-      (document.documentElement && document.documentElement.scrollTop) ||
-      document.body.scrollTop;
-    // actual page height
-    // accounting for cases where html/body are set to height:100%
-    const scrollHeight =
-      (document.documentElement && document.documentElement.scrollHeight) ||
-      document.body.scrollHeight;
-    // checker whether user scrolled to bottom of the page
-    // There can be some problems due to scroll bar thats why Math.ceil and >=
-    const scrolledToBottom =
-      Math.ceil(scrollTop + window.innerHeight) >= scrollHeight;
-
-    if (scrolledToBottom && this.startIndex <= this.totalItemsFoundInAPI) {
-      this.runDataLoad(true);
+    async fetchData(search) {
+      const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=10&startIndex=${
+        this.startIndex
+      }&orderBy=newest&fields=totalItems,items(volumeInfo/title,volumeInfo/authors,volumeInfo/industryIdentifiers,volumeInfo/imageLinks,volumeInfo/description,volumeInfo/publishedDate)`;
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      this.totalItemsFoundInAPI = data.totalItems;
+      const booksData = this.parseData(data);
+      return booksData;
     }
-  }
 
-  async performSearch() {
-    const items = await this.fetchData('done');
-    this.addBooksToPage(items);
-  }
+    parseData(data) {
+      return data.items.map(item => {
+        const thumbnail = this.getThumbnail(item);
+        const { title, publishedDate } = item.volumeInfo;
+        const isbn = this.getISBN(item);
+        const description = this.getDescription(item);
+        const author = this.getAuthor(item);
+        return {
+          thumbnail,
+          title,
+          publishedDate,
+          isbn,
+          description,
+          author,
+        };
+      });
+    }
 
-  async fetchData(search) {
-    const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=10&startIndex=${
-      this.startIndex
-    }&orderBy=newest&fields=totalItems,items(volumeInfo/title,volumeInfo/authors,volumeInfo/industryIdentifiers,volumeInfo/imageLinks,volumeInfo/description,volumeInfo/publishedDate)`;
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    this.totalItemsFoundInAPI = data.totalItems;
-    const booksData = this.parseData(data);
-    return booksData;
-  }
-
-  parseData(data) {
-    return data.items.map(item => {
-      const thumbnail = this.getThumbnail(item);
-      const { title, publishedDate } = item.volumeInfo;
-      const isbn = this.getISBN(item);
-      const description = this.getDescription(item);
-      const author = this.getAuthor(item);
-      return {
-        thumbnail,
-        title,
-        publishedDate,
-        isbn,
-        description,
-        author,
-      };
-    });
-  }
-
-  createBooksLiteral(books) {
-    let literal = ``;
-    for (const book of books) {
-      literal += `
+    createBooksLiteral(books) {
+      let literal = ``;
+      for (const book of books) {
+        literal += `
       <div class="book-wrapper">
       <div class="book-thumbnail">
           <img src=${book.thumbnail}>
@@ -101,106 +105,75 @@ class Books {
           <p class="book-description">${book.description}</p>
       </div>
   </div>`;
+      }
+      return literal;
     }
-    return literal;
-  }
 
-  addBooksToPage(books) {
-    const tempLiteralBooks = this.createBooksLiteral(books);
-    this.root.querySelector('#results').innerHTML = tempLiteralBooks;
-  }
+    addBooksToPage(books) {
+      this.output += this.createBooksLiteral(books);
+      this.root.querySelector('#results').innerHTML = this.output;
+    }
 
-  loadData(search) {
-    const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=10&startIndex=${
-      this.startIndex
-    }&orderBy=newest&fields=totalItems,items(volumeInfo/title,volumeInfo/authors,volumeInfo/industryIdentifiers,volumeInfo/imageLinks,volumeInfo/description,volumeInfo/publishedDate)`;
-    // start pulling data from API endpoint
-    fetch(endpoint)
-      .then(resp => resp.json())
-      .then(data => {
-        this.totalItemsFoundInAPI = data.totalItems;
-        // iterate through each object in result
-        // and create output out of each object property
-        data.items.forEach(item => {
-          this.output += `
-                    <div class="book-wrapper">
-                        <div class="book-thumbnail">
-                            <img src=${this.getThumbnail(item)}>
-                        </div>
-                        <div class="book-details">
-                            <h1>${this.getTitle(item)}</h1>
-                            <h3>by: ${this.getAuthor(item)}</h3>
-                            <p class="book-date">Published: ${this.getPublishedDate(
-                              item
-                            )}</p>
-                            <p class="book-isbn">ISBN: ${this.getISBN(item)}</p>
-                            <p class="book-description">${this.getDescription(
-                              item
-                            )}</p>
-                        </div>
-                    </div>`;
-          document.querySelector('#results').innerHTML = this.output;
-        });
-      })
-      .catch(err => console.log(err));
-  }
+    getISBN(data) {
+      // checks whether ISBN information exists in passed object
+      // if yes then filter ISBN information to include only ISBN_13
+      try {
+        const filteredISBN = data.volumeInfo.industryIdentifiers.filter(
+          ident => ident.type.toLowerCase() === 'isbn_13'
+        );
+        return filteredISBN.length !== 0 ? filteredISBN[0].identifier : '';
+      } catch (err) {
+        return 'missing';
+      }
+    }
 
-  getISBN(data) {
-    // checks whether ISBN information exists in passed object
-    // if yes then filter ISBN information to include only ISBN_13
-    if (data.volumeInfo.industryIdentifiers[0].identifier) {
-      const filteredISBN = data.volumeInfo.industryIdentifiers.filter(
-        ident => ident.type.toLowerCase() === 'isbn_13'
+    getDescription(data) {
+      // checks whether description information exists in passed object
+      // return only first 40 words as a description
+      try {
+        return `${data.volumeInfo.description
+          .split(' ')
+          .slice(0, 39)
+          .join(' ')}...`;
+      } catch (err) {
+        return '';
+      }
+    }
+
+    getThumbnail(data) {
+      // checks whether thumbnail link exists in passed object
+      try {
+        return data.volumeInfo.imageLinks.thumbnail;
+      } catch (err) {
+        return 'http://placehold.it/128x198';
+      }
+    }
+
+    getAuthor(data) {
+      // checks whether author information exists in passed object
+      // return each author on the same line separated by comma
+      try {
+        return data.volumeInfo.authors.join(', ');
+      } catch (err) {
+        return '';
+      }
+    }
+
+    attachEvents() {
+      // added scroll event to window
+      // checks whether user scrolled to bottom of the page
+      // and adds new content
+      window.addEventListener('scroll', () => this.checkIfScrollHitBottom());
+      this.root.addEventListener('keydown', e => {
+        if (e.keyCode === 13) {
+          this.performSearch(false);
+        }
+      });
+      this.searchButton.addEventListener('click', () =>
+        this.performSearch(false)
       );
-      return filteredISBN.length !== 0 ? filteredISBN[0].identifier : '';
-    }
-    return 'missing';
-  }
-
-  getDescription(data) {
-    // checks whether description information exists in passed object
-    // return only first 40 words as a description
-    return (
-      `${data.volumeInfo.description
-        .split(' ')
-        .slice(0, 39)
-        .join(' ')}...` || '...'
-    );
-  }
-
-  getThumbnail(data) {
-    // checks whether thumbnail link exists in passed object
-    try {
-      return data.volumeInfo.imageLinks.thumbnail;
-    } catch (err) {
-      return 'http://placehold.it/128x198';
     }
   }
-
-  getAuthor(data) {
-    // checks whether author information exists in passed object
-    // return each author on the same line separated by comma
-    return data.volumeInfo.authors.join(', ') || '';
-  }
+  const bookComponent = new Books(document);
+  bookComponent.attachEvents();
 }
-
-const book = new Books(document);
-
-// added scroll event to window
-window.addEventListener('scroll', function() {
-  // checks whether user scrolled to bottom of the page
-  // and adds new content
-  book.checkIfScrollHitBottom();
-});
-
-// add search functionality when enter is pressed
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode === 13) {
-    book.runDataLoad(false);
-  }
-});
-
-// run loading data on click
-searchButton.addEventListener('click', function() {
-  book.runDataLoad(false);
-});
